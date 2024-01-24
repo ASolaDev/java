da68eb22847ffb0596b5e50f5d9d8e2b4a51ee47
Antonio <antoniosola991@gmail.com>
2024-01-24 17:41:33 +0100

    Añadidos inferencia, constantes y casting en el paquete variables

---
+++ src/main/java/variables/Constantes.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class Constantes {
+
+    public static void main(String[] args) {
+        final float PI = 3.141592f;
+        //PI = 3;
+        
+        final int UNIDAD = 1;
+        //UNIDAD = 2;
+        
+        final String MENSAJE = "Hola mundo";
+        //MENSAJE = "Adiós mundo cruel";
+    }
+
+}
--- src/main/java/variables/OperadoresAritmeticos.java
+++ src/main/java/variables/OperadoresAritmeticos.java
@@ -4,6 +4,11 @@ public class OperadoresAritmeticos {
 
     public static void main(String[] args) {
         
+        //Operadores aritméticos
+        int i1 = 7;
+        int i2 = 5;
+        int i3 = i1 + i2;
+        System.out.println(i3);
     }
 
 }
