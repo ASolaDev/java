acec37e49808cbb523e0ec008f16e5d66baff8cf
Antonio <antoniosola991@gmail.com>
2024-01-24 13:03:59 +0100

    Inicio del proyecto

---
+++ src/main/java/com/mycompany/java/Java.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.java;
+
+/**
+ *
+ * @author Antonio
+ */
+public class Java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
