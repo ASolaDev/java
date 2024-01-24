f8b8fc472c78206f1a5266f2fefbfd119debb05b
Antonio <antoniosola991@gmail.com>
2024-01-24 18:30:28 +0100

    Arreglada carpeta Variables

---
+++ src/main/java/Variables/InferenciaDatos.java
@@ -0,0 +1,11 @@
+package Variables;
+
+public class InferenciaDatos {
+    
+    public static void main(String[] args) {
+        var cadena = "Hola a todos";
+        var x = 7;
+        var decimal = 123.456;
+    }
+    
+}
---
+++ src/main/java/Variables/Variables.java
@@ -0,0 +1,10 @@
+package Variables;
+
+public class Variables {
+    public static void main(String[] args) {
+        String msg = "Hola Mundo";
+        int x;
+        x = 7;
+        msg = "Hasta luego";
+    }
+}
