f4a0ac14de4efbf136abab40f9b45f2a893d09bf
Antonio <antoniosola991@gmail.com>
2024-04-18 18:15:07 +0200

    Añadidos

--- src/main/java/clases/App.java
+++ src/main/java/clases/App.java
@@ -5,5 +5,6 @@ public class App {
     public static void main(String[] args) {
         Coche coche = new Coche("Ford", "Focus", 2020);
         coche.arrancar();
+        coche.apagar();
     }
 }
