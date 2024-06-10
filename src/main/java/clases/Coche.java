73208f06cb5a4e7838df7f3d380ba4c63050977f
antonio <antoniosola991@gmail.com>
2024-06-10 13:08:46 +0200

    Añadidos cambios en OOP

--- src/main/java/clases/Coche.java
+++ src/main/java/clases/Coche.java
@@ -5,16 +5,30 @@ public class Coche {
     private String modelo;
     private int anio;
 
+    // Constructor
     public Coche(String marca, String modelo, int anio) {
         this.marca = marca;
         this.modelo = modelo;
         this.anio = anio;
     }
-
+    
+    // Constructor que llama al primer constructor
+    public Coche(String marca, String modelo) {
+        this(marca, modelo, 2020);
+    }
+    
+    // Constructor que llama al primer constructor
+    public Coche(String marca, int anio) {
+        this.marca = marca;
+        this.anio = anio;
+    }
+    
+    // Método de la clase
     public void arrancar() {
         System.out.println("El coche ha arrancado");
     }
-
+    
+    // Método de la clase
     public void apagar() {
         System.out.println("El coche se ha parado");
     }
