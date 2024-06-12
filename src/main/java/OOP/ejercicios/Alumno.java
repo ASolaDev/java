3a39ae4e9775bd19e57804678ee2516ce52dfb72
antonio <antoniosola991@gmail.com>
2024-06-12 12:37:10 +0200

    Más cambios

---
+++ src/main/java/OOP/ejercicios/Alumno.java
@@ -0,0 +1,114 @@
+package OOP.ejercicios;
+
+import java.util.Objects;
+
+public class Alumno {
+    private String nombre;
+    private String apellidos;
+    private String email;
+    private int edad;
+    private String telefono;
+    
+    public Alumno() {}
+    
+    public Alumno(String nombre, String apellidos, int edad) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+    public Alumno(String nombre, String apellidos, String email, int edad, String telefono) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.edad = edad;
+        this.telefono = telefono;
+    }
+    
+    public String getNombre() {
+        return this.nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String getApellidos() {
+        return this.apellidos;
+    }
+    
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    
+    public String getEmail() {
+        return this.email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public int getEdad() {
+        return this.edad;
+    }
+    
+    public void setEdad(int edad) {
+        this.edad= edad;
+    }
+    
+    public String getTelefono() {
+        return this.telefono;
+    }
+    
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.nombre);
+        hash = 53 * hash + Objects.hashCode(this.apellidos);
+        hash = 53 * hash + Objects.hashCode(this.email);
+        hash = 53 * hash + this.edad;
+        hash = 53 * hash + Objects.hashCode(this.telefono);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Alumno other = (Alumno) obj;
+        if (this.edad != other.edad) {
+            return false;
+        }
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        if (!Objects.equals(this.apellidos, other.apellidos)) {
+            return false;
+        }
+        if (!Objects.equals(this.email, other.email)) {
+            return false;
+        }
+        return Objects.equals(this.telefono, other.telefono);
+    }
+    
+    public void esMayorDeEdad(int edad) {
+        if (edad >= 18) {
+            System.out.println("El alumno %s %s es mayor de edad".formatted(nombre, apellidos));
+        } else {
+            System.out.println("El alumno %s %s es menor de edad".formatted(nombre, apellidos));
+        }
+    }
+    
+}
---
+++ src/main/java/OOP/ejercicios/Ejercicio1.java
@@ -0,0 +1,23 @@
+package OOP.ejercicios;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        Alumno alumno1 = new Alumno();
+        alumno1.setNombre("Antonio");
+        alumno1.setApellidos("Sola Avilés");
+        alumno1.setEdad(18);
+        alumno1.setEmail("antoniosola991@gmail.com");
+        alumno1.setTelefono("696616239");
+        
+        Alumno alumno2 = new Alumno();
+        alumno2.setNombre("Alejandra");
+        alumno2.setApellidos("Martí Tortorella");
+        alumno2.setEdad(17);
+        alumno2.setEmail("blablabla@gmail.com");
+        alumno2.setTelefono("587295394");
+        
+        
+    }
+
+}
